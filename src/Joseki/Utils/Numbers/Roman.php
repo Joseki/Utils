99494c7695e27403f3777947eea9ea81263ca1bf
castamir <miras.paulik@email.cz>
2015-11-15 15:15:31 +0100

    Numbers\Roman: added converter from decadic to roman numbers and via versa

---
+++ src/Joseki/Utils/Numbers/Roman.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Netiso\Joseki\Numbers;
+
+use Nette\Utils\Strings;
+
+class Roman
+{
+    public static function encode($number)
+    {
+        if ($number < 0 || $number > 3999) {
+            throw new \InvalidArgumentException("Value must be in the range 0 - 3.999, '$number' given.");
+        }
+        if ($number === 0) {
+            return 'N';
+        }
+
+        $items = [
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        ];
+
+        $result = '';
+        foreach ($items as $symbol => $value) {
+            while ($number >= $value) {
+                $result .= $symbol;
+                $number -= $value;
+            }
+        }
+
+        return $result;
+    }
+
+
+
+    public static function decode($string)
+    {
+        $digits = [
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+        ];
+        $string = Strings::upper($string);
+
+        if (!Strings::match($string, '#[IVXLCDMN]+#')) {
+            throw new \InvalidArgumentException('Malformed symbol detected. Allowed symbols are: IVXLCDMN');
+        }
+
+        if (count(explode('V', $string)) > 2 || count(explode('L', $string)) > 2 || count(explode('D', $string)) > 2) {
+            throw new \InvalidArgumentException('Multiple occurencies of V, L or D symbols');
+        }
+
+        if ($string === 'N') {
+            return 0;
+        }
+
+        $count = 1;
+        $last = 'Z';
+        foreach (str_split($string) as $char) {
+            if ($char === $last) {
+                $count++;
+                if ($count === 4) {
+                    throw new \InvalidArgumentException('Malformed Roman number');
+                }
+            } else {
+                $count = 1;
+                $last = $char;
+            }
+        }
+
+        $ptr = 0;
+        $values = [];
+        $maxDigit = 1000;
+        while ($ptr < strlen($string)) {
+            $numeral = $string[$ptr];
+            $digit = $digits[$numeral];
+
+            if ($digit > $maxDigit) {
+                throw new \InvalidArgumentException('Rule 3');
+            }
+
+            if ($ptr < strlen($string) - 1) {
+                $nextNumeral = $string[$ptr + 1];
+                $nextDigit = $digits[$nextNumeral];
+
+                if ($nextDigit > $digit) {
+                    if (!in_array($numeral, ['I', 'X', 'C']) || $nextDigit > $digit * 10 || count(explode($numeral, $string)) > 2) {
+                        throw new \InvalidArgumentException('Rule 3');
+                    }
+                    $maxDigit = $digit - 1;
+                    $digit = $nextDigit - $digit;
+                    $ptr++;
+                }
+            }
+
+            $values[] = $digit;
+            $ptr++;
+        }
+
+        for ($i = 0; $i < count($values) - 1; $i++) {
+            if ($values[$i] < $values[$i + 1]) {
+                throw new \InvalidArgumentException('Rule 5');
+            }
+        }
+
+        return array_sum($values);
+    }
+}
---
+++ tests/JosekiTests/Utils/Numbers.Roman.phpt
@@ -0,0 +1,53 @@
+<?php
+
+namespace TestCases\DataTransfers;
+
+use Netiso\Joseki\Numbers\Roman;
+use Tester\Assert;
+
+require __DIR__ . '/../bootstrap.php';
+
+Assert::equal('N', Roman::encode(0));
+Assert::equal('I', Roman::encode(1));
+Assert::equal('II', Roman::encode(2));
+Assert::equal('III', Roman::encode(3));
+Assert::equal('IV', Roman::encode(4));
+Assert::equal('V', Roman::encode(5));
+Assert::equal('VI', Roman::encode(6));
+Assert::equal('VII', Roman::encode(7));
+Assert::equal('VIII', Roman::encode(8));
+Assert::equal('IX', Roman::encode(9));
+Assert::equal('X', Roman::encode(10));
+
+Assert::equal('X', Roman::encode(10));
+Assert::equal('L', Roman::encode(50));
+Assert::equal('C', Roman::encode(100));
+Assert::equal('D', Roman::encode(500));
+Assert::equal('M', Roman::encode(1000));
+
+Assert::equal('DLV', Roman::encode(555));
+Assert::equal('CDXLIV', Roman::encode(444));
+Assert::equal('DCCLXXVII', Roman::encode(777));
+
+
+Assert::equal(0, Roman::decode('N'));
+Assert::equal(1, Roman::decode('I'));
+Assert::equal(2, Roman::decode('II'));
+Assert::equal(3, Roman::decode('III'));
+Assert::equal(4, Roman::decode('IV'));
+Assert::equal(5, Roman::decode('V'));
+Assert::equal(6, Roman::decode('VI'));
+Assert::equal(7, Roman::decode('VII'));
+Assert::equal(8, Roman::decode('VIII'));
+Assert::equal(9, Roman::decode('IX'));
+Assert::equal(10, Roman::decode('X'));
+
+Assert::equal(10, Roman::decode('X'));
+Assert::equal(50, Roman::decode('L'));
+Assert::equal(100, Roman::decode('C'));
+Assert::equal(500, Roman::decode('D'));
+Assert::equal(1000, Roman::decode('M'));
+
+Assert::equal(555, Roman::decode('DLV'));
+Assert::equal(444, Roman::decode('CDXLIV'));
+Assert::equal(777, Roman::decode('DCCLXXVII'));
